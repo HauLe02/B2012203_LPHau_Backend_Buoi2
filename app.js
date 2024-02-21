fdf0f952ff27c44fd95244bec22efe61d295e1c4
Hau Le <l22phuochau@gmail.com>
2024-02-22 01:06:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
+const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts/", contactRouter);
 
 app.get("/", (req, res) => {
     res.json({
@@ -14,4 +15,14 @@ app.get("/", (req, res) => {
     })
 });
 
-module.exports = app;
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
+module.exports = app
